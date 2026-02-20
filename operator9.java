d8d4f62d858ec4eb1c16fcb5b76618b5dc858c1a
Priyanka Saktheeswaran <priyankasaktheeswaran@gmail.com>
2026-02-20 15:13:12 +0530

    Add files via upload

---
+++ operator9.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class operator9 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int usernameMatch=sc.nextInt();
+        int  passwordMatch=sc.nextInt();
+        int  accountLocked=sc.nextInt();
+        if(usernameMatch!=1 || passwordMatch!=1 || accountLocked!=0){
+            System.out.println("Failed");
+        }
+        else{
+            System.out.println("Succes");
+        }
+    }
+}
