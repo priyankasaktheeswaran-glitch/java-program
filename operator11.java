936573c566b91d556d9b9e7f17e8c8744615f19d
Priyanka Saktheeswaran <priyankasaktheeswaran@gmail.com>
2026-02-20 15:51:07 +0530

    Add files via upload

---
+++ operator11.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class operator11 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int baseBoxes=sc.nextInt();
+        int levels=sc.nextInt();
+        int capacity=baseBoxes<<levels;
+        System.out.println("total capacity="+capacity);
+    }
+}
