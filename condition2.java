41ae78ff7ca9613259240dce5c8ed6c9a84f8b96
Priyanka Saktheeswaran <priyankasaktheeswaran@gmail.com>
2026-02-21 23:27:19 +0530

    Add files via upload

---
+++ condition2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class condition2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int creditScore=sc.nextInt();
+        double annualIncome=sc.nextInt();
+        double debtToIncomeRatio=sc.nextInt();
+        char loanType=sc.next().charAt(0);
+        String decision="Approved";
+        double loanAmount=0.0;
+        
+
+    }
+}
